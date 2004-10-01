2fd7b17c93a22d7e1cac530622226990a8c55217
jaredy <>
2004-10-01 05:17:42 +0000

    add LBUF_CHOP and LBUF_SET, and include sysexits.h

--- lbuf.h
+++ lbuf.h
@@ -6,6 +6,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sysexits.h>
 
 struct lbuf {
 	int	 lb_pos;
@@ -34,10 +35,16 @@ struct lbuf {
 #define LBUF_GET(lb)							\
 	(lb).lb_buf
 
+#define LBUF_SET(lb, s)							\
+	(lb).lb_buf = (s)
+
 #define LBUF_FREE(lb)							\
 	free((lb).lb_buf)
 
 #define LBUF_RESET(lb)							\
 	(lb).lb_pos = -1						\
+
+#define LBUF_CHOP(lb)							\
+	(lb).lb_pos--
 
 #endif /* LBUF_H */
